81ebc6175828541dd79b3730ffbe7fb25af56f91
Matheus Mota <matheuzwork@gmail.com>
2024-01-18 14:53:49 -0300

    feat: footer

--- src/app/component/Footer.tsx
+++ src/app/component/Footer.tsx
@@ -3,9 +3,9 @@ import Container from './Container'
 
 function Footer() {
   return (
-    <section className='bg-black h-24 w-full'>
-        <Container>
-            <div></div>
+    <section className='bg-black h-16 w-full'>
+        <Container className='flex items-center justify-center h-full'>
+            <h1 className=' text-white text-center '>© 2024 Matheus Mota. Todos os direitos reservados.</h1>
         </Container>
     </section>
   )
